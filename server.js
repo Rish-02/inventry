a05176cee4e4864c61515747d88094c55c099345
Rish-02 <rish.jynt03@gmail.com>
2025-02-05 16:31:28 +0530

    corsissues

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["https://inventory-rish-api.netlify.app"],
+    origin: ["https://rish-inventoryapp.netlify.app"],
     credentials: true,
   })
 );
