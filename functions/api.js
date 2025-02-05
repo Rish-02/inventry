50b225f03ea5747a6f3e833a1147dd656278f6f7
Rish-02 <rish.jynt03@gmail.com>
2025-02-05 15:54:43 +0530

    cors fix

--- functions/api.js
+++ functions/api.js
@@ -3,3 +3,14 @@ import serverless from "serverless-http";
 import { app } from "../server"
 
 export const handler = serverless(app);
+
+exports.handler = async (event, context) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "https://rish-inventoryapp.netlify.app/"
+      },
+      body: JSON.stringify({ message: "Inventory application" })
+    };
+    return response;
+  };
