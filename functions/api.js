a30de7d572c99b79a238e4e8eead31563353fd85
Rish-02 <rish.jynt03@gmail.com>
2025-02-05 16:45:20 +0530

    cors

--- functions/api.js
+++ functions/api.js
@@ -1,11 +1,10 @@
 import express, { Router } from "express";
 import serverlessHttp from "serverless-http";
-import { app } from "../server"
+import { app } from "../app"
 
 const handler = serverlessHttp(app)
 
 module.exports.handler = async(event, context)=>{
   const result = await handler(event, context);
-  result.headers.set("Access-Control-Allow-Origin", "*")
   return result;
 }
