468cf0864f571bb421a9ac095f6454c9db8897a1
Rish-02 <rish.jynt03@gmail.com>
2025-02-05 22:30:34 +0530

    eliminating cors

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     credentials: true,
+//   })
+// );
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
